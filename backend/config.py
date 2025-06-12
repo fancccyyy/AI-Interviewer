82c5c1cc551c9d319f5edf65154c1d99ff4bc183
fancccyyy <63235914+fancccyyy@users.noreply.github.com>
2025-06-13 00:05:27 +0800

    Safe Config file without API secrets

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+# config.py
+
+DASHSCOPE_API_KEY = ""
+MODEL_NAME = 'paraformer-realtime-v2'
+SAMPLE_RATE = 16000
+AUDIO_FORMAT = 'pcm'
+
+DASHSCOPE_AI_AGENTS_APP_ID = {
+    'first_hello': '',
+    'more_questions': '',
+    'switch_question': '',
+    'thats_all': ''
+}
+
+QUESTIONS = [
+    
+]
